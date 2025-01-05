3b290d5da59c72c1fe87d761f6e52c4a355989af
roney ruiz <roneyruizrojas@gmail.com>
2025-01-04 22:18:03 -0600

    feat(server): create product model and migration

---
+++ server/prisma/migrations/20250105041010_create_product_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_code_key" ON "Product"("code");
