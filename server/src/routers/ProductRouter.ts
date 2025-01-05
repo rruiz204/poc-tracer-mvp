f11d3446c3fc41aeaaf26183341993a5970ed1e9
roney ruiz <roneyruizrojas@gmail.com>
2025-01-05 12:19:54 -0600

    chore(server): remove to rename

--- server/src/routers/ProductRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { ProductController } from "@controllers/ProductController";
-
-const controller = new ProductController();
-
-export const ProductRouter = Router();
-
-ProductRouter.get("/index", controller.index);
-ProductRouter.post("/store", controller.store);
