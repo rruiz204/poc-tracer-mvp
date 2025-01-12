11a7478f432ae3d7c91bd37503a2903a82aeeebf
roney ruiz <roneyruizrojas@gmail.com>
2025-01-12 00:09:42 -0600

    feat(server): write unit testing for create product use case

--- server/src/Tests/Unit/UseCases/Product/CreateProductUseCase.spec.ts
+++ server/src/Tests/Unit/UseCases/Product/CreateProductUseCase.spec.ts
@@ -1,13 +1,24 @@
 import { describe, expect, it, vi } from "vitest";
 
-import { ValidationService } from "@Services/ValidationService";
+import { Context } from "@Database/Context";
+import { ProductFactory } from "@Database/Factories/ProductFactory";
 import { ProductRepository } from "@Repositories/ProductRepository";
 import { CreateProductUseCase } from "@UseCases/Product/CreateProduct/CreateProductUseCase";
 
-import type { CreateProductCommand } from "@UseCases/Product/CreateProduct/CreateProductCommand";
-import type { CreateProductResponse } from "@UseCases/Product/CreateProduct/CreateProductResponse";
-
 
 describe("create product use case", () => {
-  
+  const repository = new ProductRepository(Context);
+  const useCase = new CreateProductUseCase(repository);
+
+  it("should store the product if validation and checks pass", async () => {
+    const product = ProductFactory.build({ id: 1, name: "testing product" });
+    const { name, description, price, stock } = product;
+
+    vi.spyOn(repository, "find").mockResolvedValue(null);
+    vi.spyOn(repository, "store").mockResolvedValue(product);
+
+    const response = await useCase.use({ name, description, price, stock });
+    expect(response.id).toEqual(product.id);
+  });
+
 });
