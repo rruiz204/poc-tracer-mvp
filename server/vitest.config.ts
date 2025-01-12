0b570034b3cdaec155b9b7cf0a9e7e3d0cb1a2c4
roney ruiz <roneyruizrojas@gmail.com>
2025-01-12 00:09:13 -0600

    fix(server): ts alias for vitest

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,14 +1,23 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig, configDefaults } from "vitest/config";
 
 const excludedFiles = [
   ...configDefaults.exclude,
   "server.ts", "bootstrap.ts",
+
+  // CQR Files
+  "**/**Query.ts/**",
+  "**/**Command.ts/**",
+  "**/**Response.ts/**",
+
+  // Other Components
   "**/Controllers/**",
   "**/Database/**",
   "**/Routers/**",
 ];
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     coverage: {
       provider: "v8",
