9870a09e04375002c8677f6f4b57732835f705b0
roney ruiz <roneyruizrojas@gmail.com>
2025-01-11 21:07:45 -0600

    fix(server): typing

--- server/src/Database/Factories/ProductFactory.ts
+++ server/src/Database/Factories/ProductFactory.ts
@@ -8,13 +8,11 @@ interface FactoryArgs {
 
 export class ProductFactory {
   public static build({ id, name }: FactoryArgs): Product {
-    const price = faker.number.float({ min: 10, max: 50 }).toFixed(2)
-
     return {
       id: id,
       name: name,
       description: faker.lorem.paragraph(),
-      price: new Prisma.Decimal(price),
+      price: faker.number.int({ min: 10, max: 50 }),
       stock: faker.number.int({ min: 10, max: 50 }),
       active: faker.datatype.boolean(),
       createdAt: faker.date.anytime(),
