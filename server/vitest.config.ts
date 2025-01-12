ec647d9ce430aa4168dd78710ee80bbcb81a1d6a
roney ruiz <roneyruizrojas@gmail.com>
2025-01-11 21:28:07 -0600

    feat(server): setup vitest configuration

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
+
+const excludedFiles = [
+  ...configDefaults.exclude,
+  "server.ts", "bootstrap.ts",
+  "**/Controllers/**",
+  "**/Database/**",
+  "**/Routers/**",
+];
 
 export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
+      exclude: excludedFiles,
     },
+    exclude: excludedFiles,
   },
 });
