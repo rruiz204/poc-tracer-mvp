1aba0a3bbb1ebe0a2113c766061a5e47013fcb9d
roney ruiz <roneyruizrojas@gmail.com>
2025-01-11 21:36:06 -0600

    quick(server): quick save of create product use case test

---
+++ server/src/Tests/Unit/UseCases/Product/CreateProductUseCase.spec.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it, vi } from "vitest";
+
+import { ValidationService } from "@Services/ValidationService";
+import { ProductRepository } from "@Repositories/ProductRepository";
+import { CreateProductUseCase } from "@UseCases/Product/CreateProduct/CreateProductUseCase";
+
+import type { CreateProductCommand } from "@UseCases/Product/CreateProduct/CreateProductCommand";
+import type { CreateProductResponse } from "@UseCases/Product/CreateProduct/CreateProductResponse";
+
+
+describe("create product use case", () => {
+  
+});
