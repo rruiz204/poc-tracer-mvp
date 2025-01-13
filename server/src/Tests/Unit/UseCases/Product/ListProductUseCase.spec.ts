2722c9c701e590646a13575016e071499d2646f9
roney ruiz <roneyruizrojas@gmail.com>
2025-01-12 21:30:42 -0600

    feat(server): write unit testing for list produt use case

---
+++ server/src/Tests/Unit/UseCases/Product/ListProductUseCase.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it, vi } from "vitest";
+
+import { Context } from "@Database/Context";
+import { ProductFactory } from "@Database/Factories/ProductFactory";
+import { ProductRepository } from "@Repositories/ProductRepository";
+import { ListProductUseCase } from "@UseCases/Product/ListProduct/ListProductUseCase";
+
+describe("list products use case", () => {
+  const repository = new ProductRepository(Context);
+  const useCase = new ListProductUseCase(repository);
+
+  it("", async () => {
+    const firstProduct = ProductFactory.build({ id: 1, name: "first product" });
+    const secondProduct = ProductFactory.build({ id: 2, name: "second product" });
+
+    vi.spyOn(repository, "list").mockResolvedValue([firstProduct, secondProduct]);
+    const response = await useCase.use();
+    expect(response.length).toEqual(2);
+  });
+
+  it("", async () => {
+    vi.spyOn(repository, "list").mockResolvedValue([]);
+    const response = await useCase.use();
+    expect(response.length).toEqual(0);
+  });
+
+});
