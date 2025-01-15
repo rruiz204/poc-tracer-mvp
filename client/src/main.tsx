aa7d890bc2bf8d22f15931bbf51c33c529342040
roney ruiz <roneyruizrojas@gmail.com>
2025-01-14 22:20:29 -0600

    chore(client): disable strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,9 @@
-import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
 import { Router } from "./router";
 import "./index.css"
 
 const root = document.getElementById("root")!;
 
-const component = (
-  <StrictMode>
-    <Router></Router>
-  </StrictMode>
-);
-
+const component = (<Router></Router>);
 
 createRoot(root).render(component);
