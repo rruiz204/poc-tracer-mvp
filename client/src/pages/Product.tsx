4e52ae261645ed52d5d4013df9ab56da6542c476
roney ruiz <roneyruizrojas@gmail.com>
2025-01-14 20:58:13 -0600

    feat(client): create product page

---
+++ client/src/pages/Product.tsx
@@ -0,0 +1,11 @@
+import { Page } from "@shared/components/shared/Page";
+
+export const Product = () => {
+  return (
+    <Page>
+      <div className="text-white">
+        <h1 data-testid="product-title" className="text-3xl font-semibold">Product Page</h1>
+      </div>
+    </Page>
+  );
+};
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 
 import { Home } from "@pages/Home";
 import { Testing } from "@pages/Testing";
+import { Product } from "@pages/Product";
 
 export const Router = (): JSX.Element => {
   return (
@@ -9,6 +10,7 @@ export const Router = (): JSX.Element => {
       <Routes>
         <Route path="/" element={<Home></Home>} />
         <Route path="/testing" element={<Testing></Testing>} />
+        <Route path="/product" element={<Product></Product>} />
       </Routes>
     </BrowserRouter>
   );
