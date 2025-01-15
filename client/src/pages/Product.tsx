0df06a0c877400646c13627870647f79214e060e
roney ruiz <roneyruizrojas@gmail.com>
2025-01-14 23:36:35 -0600

    adjust(server): product page

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,10 +1,18 @@
 import { Page } from "@shared/components/shared/Page";
+import { useProductStore } from "@core/stores/useProductStore";
+import { useProductEffects } from "@core/effects/useProductEffects";
 
 export const Product = () => {
+  useProductEffects();
+  
+  const products = useProductStore((state) => state.products);
+
   return (
     <Page>
       <div className="text-white">
         <h1 data-testid="product-title" className="text-3xl font-semibold">Product Page</h1>
+        <hr />
+        { products.length > 0 && <p data-testid="product-counter">There are {products.length} products</p> }
       </div>
     </Page>
   );
