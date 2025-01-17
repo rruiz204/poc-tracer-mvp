8c97d750257b0c01d45925f70ca0085944598c58
roney ruiz <roneyruizrojas@gmail.com>
2025-01-16 23:20:53 -0600

    feat(server): create product seeder

--- server/src/Database/Factories/ProductFactory.ts
+++ server/src/Database/Factories/ProductFactory.ts
@@ -11,7 +11,7 @@ export class ProductFactory {
       id: id,
       name: faker.commerce.product(),
       description: faker.lorem.paragraph(),
-      price: faker.number.int({ min: 10, max: 50 }),
+      price: faker.number.int({ min: 1000, max: 20000 }),
       stock: faker.number.int({ min: 10, max: 50 }),
       active: faker.datatype.boolean(),
       createdAt: faker.date.anytime(),
