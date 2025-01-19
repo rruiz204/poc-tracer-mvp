884a5eaa01f537a252b69a680076457323ce0b29
roney ruiz <roneyruizrojas@gmail.com>
2025-01-18 23:14:31 -0600

    feat(client): create product form

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,26 +1,20 @@
-import { Page } from "@shared/components/shared/Page";
-import { useProductStore } from "@core/stores/useProductStore";
 import { useProductEffects } from "@core/effects/useProductEffects";
-import { ProductCard } from "@shared/components/product/ProductCard";
+import { ProductList } from "@shared/components/product/ProductList";
+import { ProductForm } from "@shared/components/product/ProductForm";
 
-export const Product = () => {
+export const Product = (): JSX.Element => {
   useProductEffects();
-  
-  const products = useProductStore((state) => state.products);
 
   return (
-    <Page>
-      <div className="text-white">
-        <h1 className="text-3xl font-semibold pb-4" data-testid="product-title">
-          Product Page
-        </h1>
+    <div className="bg-cs-blue-900 min-h-screen w-screen py-5 px-9">
+      <h1 className="text-3xl font-semibold mb-8 text-white">
+        Products
+      </h1>
 
-        <div className="w-full my-4 flex flex-wrap gap-4">
-          {products.map((product )=> (
-            <ProductCard product={product}></ProductCard>
-          ))}
-        </div>
+      <div className="grid gap-5 grid-cols-[2fr_1fr] text-white">
+        <ProductList></ProductList>
+        <ProductForm></ProductForm>
       </div>
-    </Page>
+    </div>
   );
 };
