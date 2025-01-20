8a603bf4c1092ea6f2f84ed08270ac0574844576
roney ruiz <roneyruizrojas@gmail.com>
2025-01-19 21:36:44 -0600

    feat(client): add role prop for button component

--- client/src/shared/components/product/ProductCard.tsx
+++ client/src/shared/components/product/ProductCard.tsx
@@ -26,8 +26,8 @@ export const ProductCard = ({ product }: Props) => {
       </div>
 
       <div className="flex gap-2 mb-2">
-        <Button text="Edit" icon={EditIcon}></Button>
-        <Button text="Remove" icon={RemoveIcon}></Button>
+        <Button text="Edit" icon={EditIcon} role="button"></Button>
+        <Button text="Remove" icon={RemoveIcon} role="button"></Button>
       </div>
     </div>
   );
--- client/src/shared/components/shared/Button.tsx
+++ client/src/shared/components/shared/Button.tsx
@@ -1,12 +1,13 @@
 interface Props {
   text: string;
   icon: string;
+  role: "submit" | "button";
 };
 
-export const Button = ({ text, icon }: Props): JSX.Element => {
+export const Button = ({ text, icon, role }: Props): JSX.Element => {
   return (
     <div className="bg-cs-blue-700 hover:bg-cs-blue-500 duration-200 text-white rounded-lg flex-1">
-      <button role="button" className="flex justify-center items-center gap-1 w-full p-2">
+      <button role={role} className="flex justify-center items-center gap-1 w-full p-2">
         <img src={icon} height={20} width={20} />
         <p className="text-white font-semibold">{text}</p>
       </button>
