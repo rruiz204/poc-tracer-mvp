f605f189e7034c78fa34214917cd0fdcc771b066
roney ruiz <roneyruizrojas@gmail.com>
2025-01-19 22:35:37 -0600

    feat(client): create format helper

--- client/src/components/Field.tsx
+++ client/src/components/Field.tsx
@@ -1,3 +1,4 @@
+import { FormatHelper } from "@core/helpers/FormatHelper";
 import { UseFormRegister, Path, FieldValues } from "react-hook-form";
 
 interface Props<V extends FieldValues> {
@@ -11,7 +12,7 @@ interface Props<V extends FieldValues> {
 export const Field = <V extends FieldValues>({ type, label, place, register, error }: Props<V>): JSX.Element => {
   return (
     <div className="flex flex-col gap-2 flex-1">
-      <label className="text-white font-semibold">{label}</label>
+      <label className="text-white font-semibold">{FormatHelper.capitalize(label)}</label>
       <input {...register(label)} type={type} placeholder={place}
         className="outline-none text-black px-2 py-1 rounded-md" />
       { error && <p className="text-red-600">{error}</p> }
