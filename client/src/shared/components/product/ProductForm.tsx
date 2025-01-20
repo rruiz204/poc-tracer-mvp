c591f11e2b13f2e0bb982c213fc78254e325ee74
roney ruiz <roneyruizrojas@gmail.com>
2025-01-19 21:37:39 -0600

    feat(client): finish product form component

--- client/src/shared/components/product/ProductForm.tsx
+++ client/src/shared/components/product/ProductForm.tsx
@@ -1,7 +1,11 @@
 import * as yup from "yup";
 import { Field } from "../shared/Field";
+import { Button } from "../shared/Button";
+import { Checkbox } from "../shared/Checkbox";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+
+import SaveIcon from "@assets/svgs/save-icon.svg";
 
 const schema = yup.object({
   name: yup.string().required(),
@@ -14,20 +18,35 @@ const schema = yup.object({
 type inputs = yup.InferType<typeof schema>;
 
 export const ProductForm = (): JSX.Element => {
-  const { register, handleSubmit } = useForm<inputs>({ resolver: yupResolver(schema) });
+  const { register, handleSubmit, formState: { errors } } = useForm<inputs>({ resolver: yupResolver(schema) });
   const onSubmit = handleSubmit((data) => console.log(data));
 
   return (
-    <div className="border-2 border-cs-blue-500">
-      <form onSubmit={onSubmit} className="flex flex-col gap-2">
-        <Field<inputs> label="name" type="text"
-          place="Enter name" register={register} />
+    <div>
+      <form onSubmit={onSubmit} className="flex flex-col gap-4 px-4 py-6 border-2 border-cs-blue-500">
+        <div className="">
+          <Field<inputs> label="name" type="text"
+            place="Enter name" register={register} error={errors.name?.message} />
+        </div>
 
-        <Field<inputs> label="description" type="text"
-          place="Enter description" register={register} />
+        <div className="flex gap-2">
+          <Field<inputs> label="price" type="text" 
+            place="Enter price" register={register} error={errors.price?.message} />
+          <Field<inputs> label="stock" type="text"
+            place="Enter stock" register={register} error={errors.stock?.message} />
+        </div>
 
-        <Field<inputs> label="price" type="text" register={register} />
-        <Field<inputs> label="stock" type="text" register={register} />
+        <div className="flex flex-col gap-2">
+          <label className="font-semibold">Description</label>
+          <textarea rows={4} {...register("description")} placeholder="Enter description"
+            className="w-full text-black outline-none px-1"></textarea>
+          { errors.description && <p className="text-red-600">{errors.description.message}</p> }
+        </div>
+
+        <div className="flex justify-end">
+          <Checkbox<inputs> label="active" register={register}></Checkbox>
+        </div>
+        <Button text="Save" role="submit" icon={SaveIcon}></Button>
       </form>
     </div>
   );
