2b29313f10da389248b918aae09ec79e6baf3155
roney ruiz <roneyruizrojas@gmail.com>
2025-01-21 22:36:56 -0600

    feat(server): new endpoints for delete and update

--- server/src/Controllers/ProductController.ts
+++ server/src/Controllers/ProductController.ts
@@ -1,23 +1,39 @@
 import type { Request, Response } from "express";
 import type { ListProductUseCase } from "@UseCases/Product/ListProduct/ListProductUseCase";
 import type { CreateProductUseCase } from "@UseCases/Product/CreateProduct/CreateProductUseCase";
+import type { UpdateProductUseCase } from "@UseCases/Product/UpdateProduct/UpdateProductUseCase";
+import type { DeleteProductUseCase } from "@UseCases/Product/DeleteProduct/DeleteProductUseCase";
 
 export class ProductController {
   constructor(
     private listProductUseCase: ListProductUseCase,
     private createProductUseCase: CreateProductUseCase,
+    private updateProductUseCase: UpdateProductUseCase,
+    private deleteProductUseCase: DeleteProductUseCase,
   ) {
     this.list = this.list.bind(this);
-    this.store = this.store.bind(this);
+    this.create = this.create.bind(this);
+    this.update = this.update.bind(this);
+    this.delete = this.delete.bind(this);
   };
 
   public async list(req: Request, res: Response) {
-    const products = await this.listProductUseCase.use();
+    const products = await this.listProductUseCase.use({});
     res.status(200).json({ path: "index", products });
   };
 
-  public async store(req: Request, res: Response) {
+  public async create(req: Request, res: Response) {
     const product = await this.createProductUseCase.use(req.body);
-    res.status(200).json({ path: "store", product });
+    res.status(200).json({ path: "create", product });
+  };
+
+  public async update(req: Request, res: Response) {
+    const product = await this.updateProductUseCase.use(req.body);
+    res.status(200).json({ path: "update", product });
+  };
+
+  public async delete(req: Request, res: Response) {
+    const product = await this.deleteProductUseCase.use(req.body);
+    res.status(200).json({ path: "delete", product });
   };
 };
--- server/src/Routers/ProductRouter.ts
+++ server/src/Routers/ProductRouter.ts
@@ -6,15 +6,24 @@ import { ProductRepository } from "@Repositories/ProductRepository";
 
 import { ListProductUseCase } from "@UseCases/Product/ListProduct/ListProductUseCase";
 import { CreateProductUseCase } from "@UseCases/Product/CreateProduct/CreateProductUseCase";
+import { UpdateProductUseCase } from "@UseCases/Product/UpdateProduct/UpdateProductUseCase";
+import { DeleteProductUseCase } from "@UseCases/Product/DeleteProduct/DeleteProductUseCase";
 
 const repository = new ProductRepository(Context);
 
 const listProductUseCase = new ListProductUseCase(repository);
 const createProductUseCase = new CreateProductUseCase(repository);
+const updateProductUseCase = new UpdateProductUseCase(repository);
+const deleteProductUseCase = new DeleteProductUseCase(repository);
 
-const controller = new ProductController(listProductUseCase, createProductUseCase);
+const controller = new ProductController(
+  listProductUseCase, createProductUseCase,
+  updateProductUseCase, deleteProductUseCase,
+);
 
 export const ProductRouter = Router();
 
-ProductRouter.get("/list", controller.list);
-ProductRouter.post("/store", controller.store);
+ProductRouter.get("/", controller.list);
+ProductRouter.post("/", controller.create);
+ProductRouter.put("/", controller.update);
+ProductRouter.delete("/", controller.delete);
