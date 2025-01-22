d332ef785c5e8974ca7a7dc6d275afc765132569
roney ruiz <roneyruizrojas@gmail.com>
2025-01-21 22:36:20 -0600

    feat(server): delete product use case

---
+++ server/src/UseCases/Product/DeleteProduct/DeleteProductUseCase.ts
@@ -0,0 +1,26 @@
+import type { UseCase } from "@UseCases/UseCase";
+import type { ProductDTO } from "@UseCases/DTOs/ProductDTO";
+import type { DeleteProductCommand } from "./DeleteProductCommand";
+import type { ProductRepository } from "@Repositories/ProductRepository";
+
+export class DeleteProductUseCase implements UseCase<DeleteProductCommand, ProductDTO> {
+  constructor(
+    private repository: ProductRepository,
+  ) {};
+
+  public async use(command: DeleteProductCommand): Promise<ProductDTO> {
+    const product = await this.repository.find({ id: { equals: command.id } });
+    if (!product) throw new Error("Product not found");
+
+    const deleted = await this.repository.delete({ id: command.id });
+    return {
+      id: deleted.id,
+      name: deleted.name,
+      description: deleted.description,
+      price: deleted.price,
+      stock: deleted.stock,
+      active: deleted.active,
+      createdAt: deleted.createdAt,
+    };
+  };
+};
