34156f1cb4d9586ab57fdec5ea04461f8de84274
roney ruiz <roneyruizrojas@gmail.com>
2025-01-24 21:57:38 -0600

    feat(client): create option component and fix typing

--- client/src/modules/products/ProductForm.tsx
+++ client/src/modules/products/ProductForm.tsx
@@ -1,17 +1,16 @@
+import { useForm } from "react-hook-form";
 import { Product } from "@core/models/Product";
 import { Field } from "@components/common/Field";
 import { Button } from "@components/common/Button";
 import { Checkbox } from "@components/common/Checkbox";
-
-import { useForm } from "react-hook-form";
+import { TextArea } from "@components/common/TextArea";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { ProductSchema, ProductInputs } from "./validation";
 import { useProductStore } from "@core/stores/useProductStore";
-
+import { ProductSchema, ProductPayload } from "@core/schemas/ProductSchema";
 import SaveIcon from "@assets/svgs/save-icon.svg";
 
 export const ProductForm = (): JSX.Element => {
-  const { register, handleSubmit, formState: { errors } } = useForm<ProductInputs>({
+  const { register, handleSubmit, formState: { errors } } = useForm<ProductPayload>({
     resolver: yupResolver(ProductSchema),
   });
 
@@ -26,26 +25,22 @@ export const ProductForm = (): JSX.Element => {
     <div>
       <form onSubmit={onSubmit} className="flex flex-col gap-4 px-4 py-6 border-2 border-cs-blue-500">
         <div>
-          <Field<ProductInputs> label="name" type="text"
+          <Field<ProductPayload> label="name" type="text"
             place="Enter name" register={register} error={errors.name?.message} />
         </div>
 
         <div className="flex gap-2">
-          <Field<ProductInputs> label="price" type="text"
+          <Field<ProductPayload> label="price" type="text"
             place="Enter price" register={register} error={errors.price?.message} />
-          <Field<ProductInputs> label="stock" type="text"
+          <Field<ProductPayload> label="stock" type="text"
             place="Enter stock" register={register} error={errors.stock?.message} />
         </div>
 
-        <div className="flex flex-col gap-2">
-          <label className="font-semibold">Description</label>
-          <textarea rows={4} {...register("description")} placeholder="Enter description"
-            className="w-full text-black outline-none px-2"></textarea>
-          {errors.description && <p className="text-red-600">{errors.description.message}</p>}
-        </div>
+        <TextArea rows={4} register={register} label="description" place="Enter description"
+          error={errors.description?.message} />
 
         <div className="flex justify-end">
-          <Checkbox<ProductInputs> label="active" register={register}></Checkbox>
+          <Checkbox<ProductPayload> label="active" register={register}></Checkbox>
         </div>
 
         <Button text="Save" role="submit" icon={SaveIcon} theme="primary"></Button>
