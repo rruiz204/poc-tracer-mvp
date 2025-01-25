895e511564ea564a7a78a193b6ba621adc51f8a9
roney ruiz <roneyruizrojas@gmail.com>
2025-01-24 21:58:00 -0600

    feat(client): add refresh for product list and loading

--- client/src/modules/products/ProductsList.tsx
+++ client/src/modules/products/ProductsList.tsx
@@ -1,14 +1,21 @@
 import { ProductCard } from "./ProductCard";
 import { useProductStore } from "@core/stores/useProductStore";
+import LoadingIcon from "@assets/icons/loading-icon.svg";
 
 export const ProductList = (): JSX.Element => {
-  const products = useProductStore((state) => state.products);
+  const { products, isLoading, error } = useProductStore();
 
   return (
-    <div className="grid gap-4 h-full grid-cols-3 place-items-center">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product}></ProductCard>
-      ))}
+    <div className="flex justify-center items-center">
+      {isLoading && <img src={LoadingIcon} width={60} height={60} className="pt-32" />}
+
+      {!isLoading && !error &&
+      <div className="flex flex-wrap justify-center gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product}></ProductCard>
+        ))}
+      </div>
+      }
     </div>
   );
 };
