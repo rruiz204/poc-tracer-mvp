5a0f49d976158175bb654037848d74b2fd67e494
roney ruiz <roneyruizrojas@gmail.com>
2025-01-24 20:59:09 -0600

    feat(client): add default value prop for input components

--- client/src/components/common/Checkbox.tsx
+++ client/src/components/common/Checkbox.tsx
@@ -1,15 +1,16 @@
 import { UseFormRegister, Path, FieldValues } from "react-hook-form";
 
 interface Props<V extends FieldValues> {
+  value?: any;
   label: Path<V>;
   register: UseFormRegister<V>;
 };
 
-export const Checkbox = <V extends FieldValues>({ label, register }: Props<V>) => {
+export const Checkbox = <V extends FieldValues>({ label, register, value }: Props<V>) => {
   return (
     <div className="w-max px-2">
       <label className="flex gap-2 items-center cursor-pointer font-semibold">
-        <input type="checkbox" {...register(label)}
+        <input type="checkbox" {...register(label)} value={value}
           className="appearance-none w-4 h-4 border-2 border-blue-500 rounded-sm bg-white mt-1 checked:bg-cs-blue-700" />
         Active
       </label>
