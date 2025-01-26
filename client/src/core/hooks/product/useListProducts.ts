a073a36e03a0ddfe1db10afb4f0d6a9e83d769c8
roney ruiz <roneyruizrojas@gmail.com>
2025-01-25 22:24:32 -0600

    feat(client): build right hooks

---
+++ client/src/core/hooks/product/useListProducts.ts
@@ -0,0 +1,18 @@
+import { useProductStore } from "@core/stores/useProductStore";
+import { ProductService } from "@core/services/product/ProductService";
+
+export const useListProduct = () => {
+  const { setProducts, setIsLoading, setError } = useProductStore();
+
+  const ListProductHandler = async () => {
+    setIsLoading(true);
+
+    const response = await ProductService.list();
+    if (response.error) setError(response.error);
+    if (response.data) setProducts(response.data.products);
+
+    setIsLoading(false);
+  };
+
+  return { ListProductHandler };
+};
---
+++ client/src/core/hooks/product/useUpdateProduct.ts
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { KhaosError } from "@core/services/khaos/KhaosTypes";
+import { useProductStore } from "@core/stores/useProductStore";
+import { ProductService } from "@core/services/product/ProductService";
+import { UpdateProductPayload } from "@core/services/product/ProductPayload";
+
+export const useUpdateProduct = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<KhaosError | undefined>(undefined);
+
+  const updateProduct = useProductStore((state) => state.updateProduct);
+
+  const UpdateProductHandler = async (payload: UpdateProductPayload) => {
+    setIsLoading(true);
+
+    const response = await ProductService.update(payload);
+    if (response.error) setError(response.error);
+    if (response.data) updateProduct(response.data.product);
+
+    setIsLoading(false);
+  };
+
+  return { UpdateProductHandler, isLoading, error };
+};
