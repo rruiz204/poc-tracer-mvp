01da2c04598aed65fa3f278253511af2a12258f6
roney ruiz <roneyruizrojas@gmail.com>
2025-01-26 21:52:55 -0600

    feat(client): build reusable modal component

---
+++ client/src/components/common/Modal.tsx
@@ -0,0 +1,28 @@
+interface Props {
+  title: string;
+  isOpen: boolean;
+  children: React.ReactNode;
+};
+
+interface Actions {
+  onClose: () => void;
+};
+
+export const Modal = ({ children, isOpen, onClose, title }: Props & Actions): JSX.Element | null => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-start pt-36 justify-center">
+      <div className="absolute inset-0 bg-black/50 backdrop-blur-sm"
+        onClick={onClose} />
+
+      <div className="relative bg-cs-blue-900 rounded-lg w-full max-w-2xl mx-4 z-10 p-4">
+        <div className="flex flex-col gap-4 text-white">
+          <h2 className="text-2xl font-semibold">{title}</h2>
+
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
