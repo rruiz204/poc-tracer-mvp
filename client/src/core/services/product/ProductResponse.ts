2272a64ac6c489ffe97d4148b9dea6ca5aabd50a
roney ruiz <roneyruizrojas@gmail.com>
2025-01-29 22:56:58 -0600

    refactor(client&server): restructure responses

--- client/src/core/services/product/ProductResponse.ts
+++ client/src/core/services/product/ProductResponse.ts
@@ -1,22 +1,17 @@
 import { Product } from "@core/models/Product";
 
-interface BaseProductResponse {
-  product: Product;
+export interface ListProductResponse {
   products: Product[];
 };
 
-export interface ListProductResponse extends Pick<BaseProductResponse, "products"> {
-  path: string;
+export interface CreateProductResponse {
+  product: Product;
 };
 
-export interface CreateProductResponse extends Pick<BaseProductResponse, "product"> {
-  path: string;
+export interface RemoveProductResponse {
+  product: Product;
 };
 
-export interface RemoveProductResponse extends Pick<BaseProductResponse, "product"> {
-  path: string;
-};
-
-export interface UpdateProductResponse extends Pick<BaseProductResponse, "product"> {
-  path: string;
+export interface UpdateProductResponse {
+  product: Product;
 };
