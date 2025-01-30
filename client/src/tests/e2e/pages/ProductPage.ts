579fab2e8b5adc431b1637285ad1d1a94819e9fc
roney ruiz <roneyruizrojas@gmail.com>
2025-01-29 23:01:11 -0600

    chore(client): product page first prototype

--- client/src/tests/e2e/pages/ProductPage.ts
+++ client/src/tests/e2e/pages/ProductPage.ts
@@ -1,9 +1,15 @@
 import { Page } from "@playwright/test";
 
 export class ProductPage {
+  private cardTestId = "product-card";
+
   constructor(private page: Page) {};
 
   public async navigate() {
     await this.page.goto("/product");
+  };
+
+  public async getProductCards() {
+    return await this.page.getByTestId(this.cardTestId).all();
   };
 };
