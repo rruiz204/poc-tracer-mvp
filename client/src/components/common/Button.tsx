c57b4cd3ceaec6335037ff88ea4d2fd57f7955a5
roney ruiz <roneyruizrojas@gmail.com>
2025-01-31 22:09:53 -0600

    feat(client): add test ids

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -20,10 +20,10 @@ export const Button = ({ role, icon, text, theme, handler }: Props & Actions): J
   const { bgColor, bgHover, txtColor } = ButtonThemes[theme];
 
   return (
-    <div className={`rounded-lg flex-1 duration-200 ${bgColor} ${bgHover}`}>
+    <div test-id="button" className={`rounded-lg flex-1 duration-200 ${bgColor} ${bgHover}`}>
       <button role={role} onClick={handler} className="flex justify-center items-center gap-1 w-full p-2">
-        <img src={icon} width={iconSize} height={iconSize} />
-        <p className={`font-semibold ${txtColor}`}>{text}</p>
+        <img test-id="button-icon" src={icon} width={iconSize} height={iconSize} />
+        <p test-id="button-text" className={`font-semibold ${txtColor}`}>{text}</p>
       </button>
     </div>
   );
