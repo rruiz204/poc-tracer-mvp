0055ba5070fb3f99abbd3258843d127f2a05e23f
roney ruiz <roneyruizrojas@gmail.com>
2025-03-19 23:05:45 -0600

    feat(server): improve product repository structure

---
+++ server/src/Repositories/Product/ProductRepository.ts
@@ -0,0 +1,36 @@
+import { PrismaClient, type Product } from "@prisma/client";
+
+import type {
+  ListProductsParams, DeleteProductParams,
+  CreateProductParams, UpdateProductParams,
+} from "./ProductParams";
+
+export class ProductRepository {
+  constructor(private prisma: PrismaClient) {};
+
+  public async list(params: ListProductsParams): Promise<Product[]> {
+    return await this.prisma.product.findMany({
+      take: params.limit,
+      skip: params.offset,
+    });
+  };
+
+  public async update(params: UpdateProductParams): Promise<Product> {
+    return await this.prisma.product.update({
+      data: params.update,
+      where: { id: params.id },
+    });
+  };
+
+  public async create(params: CreateProductParams): Promise<Product> {
+    return await this.prisma.product.create({ data: params.create });
+  };
+
+  public async delete(params: DeleteProductParams): Promise<Product> {
+    return await this.prisma.product.delete({ where: { id: params.id } });
+  };
+
+  public async findById(id: number): Promise<Product | null> {
+    return await this.prisma.product.findUnique({ where: { id } });
+  };
+};
