8a7b413a460a87ad478e0d2a78192803289924d3
roney ruiz <roneyruizrojas@gmail.com>
2025-03-19 23:05:58 -0600

    chore(server): adjust product seeder

--- server/src/Database/Seeders/ProductSeeder.ts
+++ server/src/Database/Seeders/ProductSeeder.ts
@@ -1,26 +1,23 @@
 import type { Seeder } from "./Seeder";
 import { ProductFactory } from "@Database/Factories/ProductFactory";
-import type { ProductRepository } from "@Repositories/ProductRepository";
+import { ProductRepository } from "@Repositories/Product/ProductRepository";
 
 export class ProductSeeder implements Seeder {
   private name: string = "product-seeder";
+  private amount: number = 10;
 
-  constructor(
-    private amount: number,
-    private repository: ProductRepository,
-  ) {};
+  constructor(private productRepository: ProductRepository) {};
 
   public getName(): string {
     return this.name;
-  }
+  };
 
   public async seed(): Promise<void> {
     for (let index = 0; index < this.amount; index++) {
       const data = ProductFactory.build({ id: index });
-      const { description, price, stock } = data;
-
       const name = `${data.name}-${index}`;
-      await this.repository.create({ name, description, price, stock });
+      
+      await this.productRepository.create({ create: { ...data, name } });
     };
   };
 };
