cc3ba3b73bae8e8e43bb97aaa0f143749953420b
roney ruiz <roneyruizrojas@gmail.com>
2025-03-31 14:38:40 -0600

    chore(server): remove old components

--- server/src/Configs/ExpressConfig.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from "yup";
-
-const schema = yup.object({
-  EXPRESS_PORT: yup.number().positive().required().transform((v) => Number(v)),
-});
-
-export const ExpressConfig = schema.validateSync(process.env);
