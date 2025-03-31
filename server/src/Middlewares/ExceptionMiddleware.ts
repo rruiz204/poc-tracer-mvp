64396454072ff9d4faf8d60c6fe37f220e7492c8
roney ruiz <roneyruizrojas@gmail.com>
2025-03-31 14:23:19 -0600

    chore(server): remove old components

--- server/src/Middlewares/ExceptionMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import type { Exception } from "@Exceptions/Exception";
-import type { Request, Response, NextFunction } from "express";
-
-export const ExceptionMiddleware = async (err: Exception, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.status).json({ message: err.message });
-};
--- server/src/Routers/PingRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import type { Request, Response } from "express";
-
-export const PingRouter = Router();
-
-PingRouter.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ ping: "pong" });
-});
