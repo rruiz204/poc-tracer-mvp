49d55e2d382ef60880cd366b272354f0f9b2fc81
roney ruiz <roneyruizrojas@gmail.com>
2025-04-02 18:48:39 -0600

    chore(client): update tailwind

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite"
-import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite";
+
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
   server: {
     port: 5000,
   },
