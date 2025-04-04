9e08cc9cd25ec1547c1fa070cbe9a1e5cf871f12
roney ruiz <roneyruizrojas@gmail.com>
2025-04-03 22:08:05 -0600

    feat(client): config ts paths resolve manually

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite";
 
-import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
+  plugins: [react(), tailwindcss(), svgr()],
   server: {
     port: 5000,
+  },
+  resolve: {
+    alias: [
+      { find: "@services", replacement: "/src/Core/Services" },
+      { find: "@schemas", replacement: "/src/Core/Schemas" },
+      { find: "@helpers", replacement: "/src/Core/Helpers" },
+      { find: "@models", replacement: "/src/Core/Models" },
+      { find: "@stores", replacement: "/src/Core/Stores" },
+      { find: "@hooks", replacement: "/src/Core/Hooks" },
+
+      { find: "@components", replacement: "/src/Components" },
+      { find: "@modules", replacement: "/src/Modules" },
+      { find: "@assets", replacement: "/src/Assets" },
+    ],
   },
 })
