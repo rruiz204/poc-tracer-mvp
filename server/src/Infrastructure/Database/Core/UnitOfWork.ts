16079e0f0270428cce8f43e393f67941d4f2623c
roney ruiz <roneyruizrojas@gmail.com>
2025-04-20 23:05:37 -0600

    feat(server): create user repository

--- server/src/Infrastructure/Database/Core/UnitOfWork.ts
+++ server/src/Infrastructure/Database/Core/UnitOfWork.ts
@@ -1,10 +1,14 @@
 import type { PrismaClient } from "@prisma/client";
+
+import { UserRepository } from "@Repositories/UserRepository";
 import { ProductRepository } from "@Repositories/Product/ProductRepository";
 
 export class UnitOfWork {
+  public user: UserRepository;
   public product: ProductRepository;
 
   constructor(context: PrismaClient) {
+    this.user = new UserRepository(context);
     this.product = new ProductRepository(context);
   };
 };
