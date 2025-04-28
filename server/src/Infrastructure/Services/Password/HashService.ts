f296960d60decf03724e6431f16003f987ccad39
roney ruiz <roneyruizrojas@gmail.com>
2025-04-27 22:17:18 -0600

    feat(server): create jwt and hash services

---
+++ server/src/Infrastructure/Services/Password/HashService.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcryptjs";
+import { AuthConfig } from "@Configs/AuthConfig";
+
+export class HashService {
+  public static async hash(password: string): Promise<string> {
+    return await bcrypt.hash(password, AuthConfig.AUTH_HASH_ROUNDS);
+  };
+
+  public static async verify(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash);
+  };
+};
