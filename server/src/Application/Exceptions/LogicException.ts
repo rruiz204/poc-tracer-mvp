a97bae7d01bea44c40edf2ecea0c50b0632bc4d4
roney ruiz <roneyruizrojas@gmail.com>
2025-04-27 22:20:53 -0600

    feat(server): create a new exception for bad credentials

--- server/src/Application/Exceptions/LogicException.ts
+++ server/src/Application/Exceptions/LogicException.ts
@@ -4,4 +4,5 @@ export class LogicException extends BaseException {
 
   public static readonly NotFound = this.fromStatus(404);
   public static readonly Redundancy = this.fromStatus(500);
+  public static readonly BadCredentials = this.fromStatus(401);
 };
