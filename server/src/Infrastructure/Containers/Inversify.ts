50091da85efaba98a3bab97e7330c35efda3dc55
roney ruiz <roneyruizrojas@gmail.com>
2025-05-15 22:04:13 -0600

    feat(server): create presentation container

--- server/src/Infrastructure/Containers/Inversify.ts
+++ server/src/Infrastructure/Containers/Inversify.ts
@@ -1,8 +1,10 @@
 import { Container } from "inversify";
 import { Application } from "./Layers/Application";
+import { Presentation } from "./Layers/Presentation";
 import { Infrastructure } from "./Layers/Infrastructure";
 
 export const Inversify = new Container();
 
 Inversify.load(Infrastructure);
 Inversify.load(Application);
+Inversify.load(Presentation);
